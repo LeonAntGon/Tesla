710c8640dcef93dfb5b1f24072d6fbd39cff0f5c
unknown <gonzalezleonardoantonio056@gmail.com>
2023-12-22 13:07:06 -0300

    added actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
-import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://LeonAntGon.github.io',
+  base: '/my-repo',
 });
